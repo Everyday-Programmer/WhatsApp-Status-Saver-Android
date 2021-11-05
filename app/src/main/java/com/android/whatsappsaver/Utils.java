47d4f8cb426b6be01a2c217df507910512dc59d1
Everyday Programmer <79357167+Everyday-Programmer@users.noreply.github.com>
2021-11-05 20:28:16 +0530

    Update Utils.java

--- app/src/main/java/com/android/whatsappsaver/Utils.java
+++ app/src/main/java/com/android/whatsappsaver/Utils.java
@@ -3,5 +3,4 @@ package com.android.whatsappsaver;
 public class Utils {
     public static final String WhatsAppDirectoryPath = "/storage/emulated/0/WhatsApp/Media/.Statuses/";
     public static final String statusSaverPath = "/storage/emulated/0/Download/WhatsAppSavedStatus/";
-    //public static final String whatsAppDirectory = "/storage/emulated/0/Download/";
 }
